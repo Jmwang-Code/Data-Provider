72a03ec81d89a6c6396362178c1fa554b4368f7a
王佳 <792836909@qq.com>
2022-10-07 02:07:09 +0800

    搭建项目

---
+++ data-provider-base/src/main/java/com/cn/jmw/data/provider/base/DataProviderBaseApplication.java
@@ -0,0 +1,13 @@
+package com.cn.jmw.data.provider.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProviderBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataProviderBaseApplication.class, args);
+    }
+
+}
