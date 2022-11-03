23a2d5d0deb568ef22cd041572ac75545b2d0729
王佳 <792836909@qq.com>
2022-11-03 19:34:15 +0800

    ES功能扩展，基本LOCAL功能支持

--- data-provider-es/src/main/java/com/cn/jmw/data/provider/es/ESProvider.java
+++ data-provider-es/src/main/java/com/cn/jmw/data/provider/es/ESProvider.java
@@ -30,6 +30,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.stream.Collectors;
 
 /**
