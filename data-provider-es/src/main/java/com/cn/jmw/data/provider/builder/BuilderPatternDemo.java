2ea3cbec25ac649e6b01d1c835cf752c5dec0ab6
王佳 <792836909@qq.com>
2022-11-16 17:22:50 +0800

    test参数创建

--- data-provider-es/src/main/java/com/cn/jmw/data/provider/builder/BuilderPatternDemo.java
+++ data-provider-es/src/main/java/com/cn/jmw/data/provider/builder/BuilderPatternDemo.java
@@ -9,18 +9,38 @@ import com.cn.jmw.data.provider.es.entity.EsRequestParam;
  * @Version 1.0
  */
 public class BuilderPatternDemo {
+
+    /**
+     *  objectObjectHashMap.put(" name ", " testOne ");
+     *         objectObjectHashMap.put("address", "152.136.154.249:9200");
+     *         objectObjectHashMap.put("scheme", "http");
+     *         objectObjectHashMap.put("query", "");
+     *         objectObjectHashMap.put("username", "");
+     *         objectObjectHashMap.put("password", "");
+     *         objectObjectHashMap.put("index", "kibana_sample_data_ecommerce");
+     *         objectObjectHashMap.put("model", "indexId");//index-id
+     *         objectObjectHashMap.put("id", "-43_74MBeofnJbrlVzm1");
+     */
     public static void main(String[] args) {
-        EsRequestParam esRequestParam = new EsRequestParam();
+        EsRequestParam esRequestParam = EsRequestParam.builder()
+                .size(3)
+                .pageStart(0)
+                .address("152.136.154.249:9200")
+                .schema("http")
+                .index("kibana_sample_data_ecommerce")
+                .id("43_74MBeofnJbrlVzm1")
+                .build();
+
         ElasticSearchBuilder elasticSearchBuilder = new ElasticSearchBuilder(esRequestParam);
 
         ElasticSearchPluginManager vegMeal = elasticSearchBuilder.prepareVegMeal(esRequestParam);
         System.out.println("Veg Meal");
         vegMeal.showItems();
-        System.out.println("Total Cost: " +vegMeal.getCost());
+        System.out.println("Total Cost: " +vegMeal.function());
 
         ElasticSearchPluginManager nonVegMeal = elasticSearchBuilder.prepareNonVegMeal(esRequestParam);
         System.out.println("\n\nNon-Veg Meal");
         nonVegMeal.showItems();
-        System.out.println("Total Cost: " +nonVegMeal.getCost());
+        System.out.println("Total Cost: " +nonVegMeal.function());
     }
 }
