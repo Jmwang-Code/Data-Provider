228c3e1faff655894ce80cae0cfb2b3c1717e2c5
王佳 <792836909@qq.com>
2022-11-17 22:58:51 +0800

    Close关闭功能

---
+++ data-provider-es/src/main/java/com/cn/jmw/data/provider/builder/plugins/Close.java
@@ -0,0 +1,12 @@
+package com.cn.jmw.data.provider.builder.plugins;
+
+/**
+ * @author jmw
+ * @Description TODO
+ * @date 2022年11月17日 14:31
+ * @Version 1.0
+ */
+public interface Close {
+
+    void clones();
+}
---
+++ data-provider-es/src/main/java/com/cn/jmw/data/provider/builder/plugins/ClosePlugin.java
@@ -0,0 +1,11 @@
+package com.cn.jmw.data.provider.builder.plugins;
+
+
+/**
+ * @author jmw
+ * @Description TODO
+ * @date 2022年11月17日 22:44
+ * @Version 1.0
+ */
+public interface ClosePlugin extends Plugins{
+}
