4d9ccb61621a87dd08b1dec37b8f1f711f037fec
王佳 <792836909@qq.com>
2022-11-17 10:59:52 +0800

    中间加一层，保存变量

---
+++ data-provider-es/src/main/java/com/cn/jmw/data/provider/builder/plugins/PluginsAbstract.java
@@ -0,0 +1,16 @@
+package com.cn.jmw.data.provider.builder.plugins;
+
+import com.cn.jmw.data.provider.es.entity.EsRequestParam;
+
+/**
+ * @author jmw
+ * @Description TODO
+ * @date 2022年11月16日 17:40
+ * @Version 1.0
+ */
+public abstract class PluginsAbstract implements Plugins{
+
+    public EsRequestParam esRequestParam;
+
+
+}
