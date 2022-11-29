0952b8b3dda46d7e9bcb18fdc69389f77f77f59b
王佳 <792836909@qq.com>
2022-11-29 18:18:22 +0800

    链式调用的提供

--- data-provider-es/src/main/java/com/cn/jmw/data/provider/builder/ElasticSearchBuilder.java
+++ data-provider-es/src/main/java/com/cn/jmw/data/provider/builder/ElasticSearchBuilder.java
@@ -1,17 +1,25 @@
 package com.cn.jmw.data.provider.builder;
 
 import com.cn.jmw.data.provider.ThreadLocalCache;
+import com.cn.jmw.data.provider.base.entity.db.Dataframe;
+import com.cn.jmw.data.provider.builder.en.Page;
+import com.cn.jmw.data.provider.builder.en.Query;
+import com.cn.jmw.data.provider.builder.en.Sort;
 import com.cn.jmw.data.provider.builder.plugins.plu.ClosePlugin;
+import com.cn.jmw.data.provider.builder.plugins.plu.SortPlugin;
 import com.cn.jmw.data.provider.builder.plugins.plu.page.FromSizePage;
+import com.cn.jmw.data.provider.builder.plugins.plu.page.ScrollPage;
 import com.cn.jmw.data.provider.builder.plugins.plu.query.AllQuery;
 import com.cn.jmw.data.provider.builder.plugins.plu.sort.Ascending;
 import com.cn.jmw.data.provider.es.entity.EsRequestParam;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -20,8 +28,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
-@Data
+@Slf4j
+//@Data
 /**
  * @author jmw
  * @Description TODO
@@ -31,6 +39,7 @@ import java.util.List;
 public class ElasticSearchBuilder {
 
     private ThreadLocalCache threadLocalCache;
+    private ElasticSearchPluginManager meal;
 
     public ElasticSearchBuilder(ThreadLocalCache threadLocalCache) {
         this.threadLocalCache = threadLocalCache;
@@ -65,15 +74,51 @@ public class ElasticSearchBuilder {
         esRequestParam.setRestHighLevelClient(new RestHighLevelClient(builder));
     }
 
-    public ElasticSearchPluginManager prepareVegMeal() {
+    /**
+     * 适配1分页方式
+     */
+    public ElasticSearchBuilder setPage(Page page){
         ElasticSearchPluginManager meal = new ElasticSearchPluginManager(threadLocalCache);
-        //TODO 中间需要加一层 去通过ES参数 判断自适应Plugins
-//        Plugins plugins = new FromSizePage(esRequestParam);
-        meal.addItem(new FromSizePage(threadLocalCache));
-        meal.addItem(new Ascending(threadLocalCache));
-        meal.addItem(new AllQuery(threadLocalCache));
-        meal.addItem(new ClosePlugin(threadLocalCache));
-        return meal;
+        this.meal = meal;
+        switch (page){
+            case FROM_SIZE -> {meal.addItem(new FromSizePage(threadLocalCache));break;}
+            case SCROLL_PAGE -> {meal.addItem(new ScrollPage(threadLocalCache));break;}
+            default -> log.error("Paging mode not recognized");
+        }
+        return this;
     }
+
+    public ElasticSearchBuilder setSort(Sort sort){
+        meal.addItem(new Ascending(threadLocalCache));
+        return this;
+    }
+
+    public ElasticSearchBuilder setQuery(Query query){
+        switch (query){
+            case INDEX_ALL -> {meal.addItem(new AllQuery(threadLocalCache));break;}
+            default -> log.error("Querying mode not recognized");
+        }
+        return this;
+    }
+
+    public SearchResponse build(){
+        meal.addItem(new ClosePlugin(threadLocalCache));
+        meal.showItems(threadLocalCache);
+
+        return (SearchResponse)threadLocalCache.get(Plugin.SEARCH_RESPONSE);
+    }
+
+
+
+//    public ElasticSearchPluginManager prepareVegMeal() {
+//        ElasticSearchPluginManager meal = new ElasticSearchPluginManager(threadLocalCache);
+//        //TODO 中间需要加一层 去通过ES参数 判断自适应Plugins
+////        Plugins plugins = new FromSizePage(esRequestParam);
+//        meal.addItem(new FromSizePage(threadLocalCache));
+//        meal.addItem(new Ascending(threadLocalCache));
+//        meal.addItem(new AllQuery(threadLocalCache));
+//        meal.addItem(new ClosePlugin(threadLocalCache));
+//        return meal;
+//    }
 
 }
